cfd7b1055ef16163f59f1d7201ea0d32389f95b3
PositionDreamer <nulva1@yandex.ru>
2026-04-07 23:45:34 +0500

    add warehouses places

--- api/admin/warehouses/create.php
+++ api/admin/warehouses/create.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../../connect.php";
 require_once __DIR__ . "/../../helpers/check_data.php";
 
-$messages = check_data(['type', 'title', 'description', 'few', 'few_very'], $_POST);
+$messages = check_data(['type', 'title', 'description', 'few', 'few_very', 'few_other', 'few_very_other'], $_POST);
 
 require_once __DIR__ . "/../../helpers/check_messages.php";
 
@@ -11,6 +11,8 @@ $title = $_POST['title'];
 $description = $_POST['description'];
 $few = $_POST['few'];
 $few_very = $_POST['few_very'];
+$few_other = $_POST['few_other'];
+$few_very_other = $_POST['few_very_other'];
 
 $check = mysqli_query($connect, "SELECT * FROM `warehouses` WHERE `title` = '$title'");
 if (mysqli_num_rows($check) > 0) {
@@ -20,7 +22,7 @@ if (mysqli_num_rows($check) > 0) {
     http_response_code(400);
     echo json_encode($req);
 } else {
-    mysqli_query($connect, "INSERT INTO `warehouses` (`id_type`, `title`, `description`, `few`, `few_very`, `hidden`) VALUES ($type, '$title', '$description', $few, $few_very, 0)");
+    mysqli_query($connect, "INSERT INTO `warehouses` (`id_type`, `title`, `description`, `few`, `few_very`, `few_other`, `few_very_other`, `hidden`) VALUES ($type, '$title', '$description', $few, $few_very, $few_other, $few_very_other, 0)");
     $last_id = mysqli_insert_id($connect);
     $req = [
         'messages' => ['Склад успешно добавлен'],
@@ -31,6 +33,8 @@ if (mysqli_num_rows($check) > 0) {
             'description' => $description,
             'few' => $few,
             'few_very' => $few_very,
+            'few_other' => $few_other,
+            'few_very_other' => $few_very_other,
         ]
     ];
     http_response_code(200);
--- api/admin/warehouses/update.php
+++ api/admin/warehouses/update.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../../connect.php";
 require_once __DIR__ . "/../../helpers/check_data.php";
 
-$messages = check_data(['id', 'type', 'title', 'description', 'few', 'few_very'], $_POST);
+$messages = check_data(['id', 'type', 'title', 'description', 'few', 'few_very', 'few_other', 'few_very_other'], $_POST);
 
 require_once __DIR__ . "/../../helpers/check_messages.php";
 
@@ -12,12 +12,14 @@ $title = $_POST['title'];
 $description = $_POST['description'];
 $few = $_POST['few'];
 $few_very = $_POST['few_very'];
+$few_other = $_POST['few_other'];
+$few_very_other = $_POST['few_very_other'];
 
 $check = mysqli_query($connect, "SELECT * FROM `warehouses` WHERE `title`='$title'");
 if (mysqli_num_rows($check) > 0) {
     $check = mysqli_fetch_assoc($check);
     if($check['id'] == $id) {
-        mysqli_query($connect, "UPDATE `warehouses` SET `id_type`=$type,`title`='$title',`description`='$description', `few` = $few, `few_very` = $few_very WHERE `id`=$id");
+        mysqli_query($connect, "UPDATE `warehouses` SET `id_type`=$type,`title`='$title',`description`='$description', `few` = $few, `few_very` = $few_very, `few_other`=$few_other, `few_very_other`=$few_very_other WHERE `id`=$id");
         $req = [
           "messages" => ["Склад успешно изменен"],
           "warehouse" => [
@@ -27,6 +29,8 @@ if (mysqli_num_rows($check) > 0) {
               "description" => $description,
               'few' => $few,
               'few_very' => $few_very,
+              'few_other' => $few_other,
+              'few_very_other' => $few_very_other,
           ]
         ];
         http_response_code(200);
@@ -39,7 +43,7 @@ if (mysqli_num_rows($check) > 0) {
         echo json_encode($req);
     }
 } else {
-    mysqli_query($connect, "UPDATE `warehouses` SET `id_type`=$type,`title`='$title',`description`='$description', `few` = $few, `few_very` = $few_very WHERE `id`=$id");
+    mysqli_query($connect, "UPDATE `warehouses` SET `id_type`=$type,`title`='$title',`description`='$description', `few` = $few, `few_very` = $few_very, `few_other`=$few_other, `few_very_other`=$few_very_other WHERE `id`=$id");
     $req = [
         "messages" => ["Склад успешно изменен"],
         "warehouse" => [
@@ -49,6 +53,8 @@ if (mysqli_num_rows($check) > 0) {
             "description" => $description,
             'few' => $few,
             'few_very' => $few_very,
+            'few_other' => $few_other,
+            'few_very_other' => $few_very_other,
         ]
     ];
     http_response_code(200);
